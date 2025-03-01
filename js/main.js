14bbc82662dc501f272c7f1b024fc722bd7b986f
Faisal Chowdhury <chowdhuryfaisal66@gmail.com>
2025-03-01 21:37:02 +0600

    Realtime History feature added

--- js/main.js
+++ js/main.js
@@ -14,43 +14,76 @@ dashboardDate.innerHTML = `
 
 // completed Button 
 
-let completedBtns =  document.querySelectorAll('.completed');
+let completedBtns = document.querySelectorAll('.completed');
 
 let assignedTask = completedBtns.length;
-document.getElementById('task-assigned').innerText = assignedTask  ;
+document.getElementById('task-assigned').innerText = assignedTask;
 
 
-for(let btn of completedBtns){
+for (let btn of completedBtns) {
+
+
+    btn.addEventListener('click', function (event) {
+
+        event.target.setAttribute('disabled', true)
+
+        // assigning to the regular task
+        assignedTask = assignedTask - 1
+        document.getElementById('task-assigned').innerText = assignedTask;
+
+
+        // assigning to the total task
+        let totalTask = parseInt(document.getElementById('total-task').innerText);
+
+        totalTask = totalTask + 1
+
+        document.getElementById('total-task').innerText = totalTask
+
+
+        // Real Updation notification with time and title 
+
+        // time
+        let hours = new Date().getHours()
+        let minutes = new Date().getMinutes()
+        let seconds = new Date().getSeconds()
+        let amOrPm = hours > 12 ? 'PM' : 'AM';
+
+        let time = `${hours} : ${minutes} : ${seconds} ${amOrPm}`
  
+
+        //Fetching title
+
+        let getTitlePath = event.target.parentNode.parentNode.getElementsByTagName('h1');
+        const getTitle = getTitlePath[0].innerHTML;
+
+        
+        // notification 
+
+        let notificationsDiv = document.getElementById('notifications');
+
+        let div = document.createElement('div');
+        div.classList.add('bg-[#F4F7FF]', 'p-3', 'rounded-md');
+
+        div.innerHTML = `
     
- btn.addEventListener('click' , function(event){
-   
-   event.target.setAttribute('disabled' , true)
-    
-   // assigning to the regular task
-   assignedTask = assignedTask - 1
-   document.getElementById('task-assigned').innerText = assignedTask;
-   
-
-   // assigning to the total task
-   let totalTask = parseInt(document.getElementById('total-task').innerText);
-
-   totalTask = totalTask + 1
-
-   document.getElementById('total-task').innerText =  totalTask
+    <p class="text-sm">You have Complete The Task <span class="bg-yellow-400"> ${getTitle}</span> at ${time}</p>
+  
+   `
+        notificationsDiv.appendChild(div)
 
 
 
-   
 
 
-   alert('Board Updated Successfully')
+        // Alert
 
-   if(assignedTask === 0){
-    alert('Congress!!! You have completed all the task ')
-   }
+        alert('Board Updated Successfully')
 
- })
+        if (assignedTask === 0) {
+            alert('Congress!!! You have completed all the task ')
+        }
+
+    })
 
 
 
